fe9cab4ac41ab66917687d03cbeada1d5d1f95f1
Dhruv Bansal <dk.bansal0026@gmail.com>
2024-10-26 11:03:11 -0700

    fix: button-ui

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,8 +9,7 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default:
-          "bg-primary text-primary-foreground hover:bg-primary/90 border border-blue-500 hover:shadow-sm",
+        default: "bg-primary text-primary-foreground hover:bg-primary/90",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
