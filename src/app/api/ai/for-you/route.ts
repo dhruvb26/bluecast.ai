3c0753f7b9f1de3ee40bf61b401b5444b5a47857
Dhruv Bansal <dk.bansal0026@gmail.com>
2024-10-28 21:58:30 -0700

    edit: prompt for for-you

--- src/app/api/ai/for-you/route.ts
+++ src/app/api/ai/for-you/route.ts
@@ -3,7 +3,6 @@ import { NextResponse } from "next/server";
 import {
   checkAccess,
   getForYouAnswers,
-  setGeneratedWords,
   getUser,
   saveForYouPosts,
 } from "@/actions/user";
@@ -93,46 +92,53 @@ export async function POST(
     const postPrompt = `You are a copywriter with 25 years of experience. You are tasked with creating 5 distinct LinkedIn posts based on the following information:
 
     <examples>
-    ${examples}
+    ${examples || ""}
     </examples>
     Study these examples carefully to understand the tone, formatting, and styling of successful LinkedIn posts. Don't pull any information from them.
-    Pay attention to recurring hooks or CTAs in the examples.
-    For example: If examples don't use bullet points, don't use them.
+    - Mimic the tone, voice, and writing style precisely
+    - Reproduce any unique patterns in content presentation
+    - DO NOT use any specific information or content from these examples
 
     <formats>
-    ${formats}
+    ${formats || ""}
     </formats>
-    Understand the user's preferred formats and use them to guide the content. For example, if the user wants to write interviews, write posts in that format.
+    Understand the user's preferred formats and use them to guide the content. 
+    - Use this format only if it doesn't conflict with the user's style.
+    - If the number of formats is less than 5, generate respective number of posts following the format and remaining posts should be formatted according to the examples above if any. 
+    - If the number of formats is more than 5, generate 5 posts following the formats.
 
     <topics>
-    ${topics}
+    ${topics || ""}
     </topics>
-    These are the topics to cover in the posts. If user has given you a list of topics, use them. If not, use the topics generated as a guide to create 6 posts.
-  
+    These are the topics to cover in the posts.
+    - If user has given you a list of topics, use them.
+    - If user has not given you a list of topics, generate 5 topics based on the user's answers below.
+
     <user_info>
-    ${aboutYourself}
+    ${aboutYourself || ""}
     </user_info>
     Analyze what the user's role might be on LinkedIn. Write posts from the perspective of the user's role.
 
     <target_audience>
-    ${targetAudience}
+    ${targetAudience || ""}
     </target_audience>
     Consider how to cater to this audience. Write posts from the perspective of the user's role.
 
     <personal_touch>
-    ${personalTouch}
+    ${personalTouch || ""}
     </personal_touch>
-    This is the user's personal writing style. Incorporate it into your posts. NOTE: This takes priority over the examples.
+    This is the user's personal writing style. Incorporate it into your posts. 
+    - NOTE: This takes PRIORITY over the examples provided above.
 
     Write 5 full LinkedIn posts following these guidelines:
 
     a. Each post should be between 1200 and 1500 characters long.
     b. Start writing each post directly without including a starting idea (one-liner) or hook. The posts should not have a title or subtitle.
-    c. Do not include emojis or hashtags unless specifically mentioned in the custom instructions.
+    c. Do not include emojis or hashtags unless specifically mentioned by user's personal touch.
     d. Emulate the tone, formatting, and styling of the analyzed examples. However, do not draw any specific information or content from these examples - they are solely for guiding the writing style.
-    e. If conflicts arise in the tone, formatting, or styling prioritize the user's personal writing prefernce over the examples. For example, if the user wants to use emojis, do so or if they don't want to use bullet points, but the example uses them, do not use bullet points.
+    e. If conflicts arise in the tone, formatting, or styling prioritize the user's personal writing preference over the examples. For example, if the user wants to use emojis, do so or if they don't want to use bullet points, but the example uses them, do not use bullet points.
 
-    Respond with 6 LinkedIn post contents only, separated by three dashes (---). Include appropriate new lines and spacing within each post. Do not include any explanations, comments, or additional formatting.`;
+    Respond with 5 LinkedIn post contents only, separated by three dashes (---). Include appropriate new lines and spacing within each post. Do not include any explanations, comments, or additional formatting.`;
 
     const postResponse = await anthropic.messages.create({
       model: env.MODEL,
