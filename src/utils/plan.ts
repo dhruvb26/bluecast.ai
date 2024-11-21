68876f972d4b9c8ec5ccf646e4a1fd962aeafe9b
Dhruv Bansal <dk.bansal0026@gmail.com>
2024-11-20 21:45:06 -0700

    add: names in team switcher

--- src/utils/plan.ts
+++ src/utils/plan.ts
@@ -1,11 +1,24 @@
 const planMap = [
-  { id: "price_1Pb0w5RrqqSKPUNWGX1T2G3O", plan: "Launch" },
-  { id: "price_1QMcQPRrqqSKPUNWXMw3yYy8", plan: "Launch" },
+  // Pro Plan - Monthly
+  { id: "price_1Q32F1RrqqSKPUNWkMQXCrVC", plan: "Pro" }, // dev
+  { id: "price_1Pb0w5RrqqSKPUNWGX1T2G3O", plan: "Pro" }, // prod
+
+  // Pro Plan - Annual
+  { id: "price_1QMOWRRrqqSKPUNWRV27Uiv7", plan: "Pro" }, // dev
+  { id: "price_1QN9MVRrqqSKPUNWHqv3bcMM", plan: "Pro" }, // prod
+
+  // Grow Plan - Monthly
+  { id: "price_1QLXONRrqqSKPUNW7s5FxANR", plan: "Grow" }, // dev
+  { id: "price_1QN9JoRrqqSKPUNWuTZBJWS1", plan: "Grow" }, // prod
+
+  // Grow Plan - Annual
+  { id: "price_1QMOYXRrqqSKPUNWcFVWJIs4", plan: "Grow" }, // dev
+  { id: "price_1QN9NyRrqqSKPUNWWwB1zAXa", plan: "Grow" }, // prod
 ];
 
 export function getPlanType(priceId: string | null): string {
   if (!priceId) return "Trial";
 
   const matchedPlan = planMap.find((item) => item.id === priceId);
-  return matchedPlan ? matchedPlan.plan : "Testing";
+  return matchedPlan ? matchedPlan.plan : "Trial";
 }
