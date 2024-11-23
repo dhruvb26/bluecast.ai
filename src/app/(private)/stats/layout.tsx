22652b5b14cb8082c5307c268298430715404fa2
Dhruv Bansal <dk.bansal0026@gmail.com>
2024-11-23 13:44:01 -0700

    add: userId to stat layout

--- src/app/(private)/stats/layout.tsx
+++ src/app/(private)/stats/layout.tsx
@@ -14,7 +14,8 @@ export default function StatsLayout({
   if (
     userId !== "user_2o3UtX2zFFgJh8HGG3quxJrRtP3" &&
     userId !== "user_2mcClELioxVoLfySM9DdYDbiSwG" &&
-    userId !== "user_2mqOdAXi5NS404SUJaSMFR2fMFN"
+    userId !== "user_2mqOdAXi5NS404SUJaSMFR2fMFN" &&
+    userId !== "user_2ntPOAWO2Jia6pS6sJkCyRLb8tj"
   ) {
     redirect("/create/posts");
   }
